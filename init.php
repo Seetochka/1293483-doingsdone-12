1641607cc2d81488f1c43dfdc42970c5755182e6
Svetlana Lopareva <seetochka95@gmail.com>
2020-06-29 19:29:48 +0300

    Добавляет страницу создания новой задачи

--- init.php
+++ init.php
@@ -1,9 +1,10 @@
 <?php
 require_once 'db.php';
+require_once 'functions.php';
 
-function get_db_connection($db_connection_data) {
-    return mysqli_connect($db_connection_data['host'], $db_connection_data['user'], $db_connection_data['password'], $db_connection_data['database']);
-}
+error_reporting(E_ALL);
+ini_set('error_log', __DIR__ . '/php-errors.log');
+error_log('Запись в лог', 0);
 
 $link = get_db_connection($db);
 mysqli_set_charset($link, "utf8");
