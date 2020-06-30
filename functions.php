29c2e0b89e925df241bd5ecb55a57622d2d1c817
Svetlana Lopareva <seetochka95@gmail.com>
2020-06-30 12:16:37 +0300

    Добавляет страницу регистрации аккаунта

--- functions.php
+++ functions.php
@@ -139,6 +139,28 @@ function is_date_correct(string $value): bool
 }
 
 /**
+ * Проверяет является ли строка корректным email
+ * @param string $value Email
+ *
+ * @return bool true при правильном email, иначе false
+ */
+function is_email(string $value): bool
+{
+    return boolval(filter_var($value, FILTER_VALIDATE_EMAIL));
+}
+
+/**
+ * Проверяет, чтобы в составе пароля были минимум одна цифра и по одной букве верхнего и нижнего регистров
+ * @param string $value Пароль
+ *
+ * @return bool true соответствии пароля условию, иначе false
+ */
+function is_strong_password(string $value): bool
+{
+    return boolval(preg_match('/^\S*(?=\S{6,128})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/', $value));
+}
+
+/**
  * Валидация заполненности поля
  * @param string $value Содержимое поля Название
  *
@@ -173,6 +195,51 @@ function validate_due_date(string $value): ?string
             break;
         case (is_date_correct($value)):
             $error_message = 'Дата должна быть больше или равна текущей';
+            break;
+    }
+
+    return $error_message;
+}
+
+/**
+ * Валидация поля Электронная почта
+ * @param string $value Содержимое поля Электронная почта
+ *
+ * @return string | null Текст ошибки или null, если валидация пройдена
+ */
+function validate_email(string $value): ?string
+{
+    $error_message = null;
+
+    switch (false) {
+        case (!empty($value)):
+            $error_message = 'Это поле должно быть заполнено';
+            break;
+        case (is_email($value)):
+            $error_message = 'Введите корректный Email';
+            break;
+    }
+
+    return $error_message;
+}
+
+/**
+ * Валидация поля Пароль
+ * @param string $value Содержимое поля Пароль
+ *
+ * @return string | null Текст ошибки или null, если валидация пройдена
+ */
+function validate_password(string $value): ?string
+{
+    $error_message = null;
+
+    switch (false) {
+        case (!empty($value)):
+            $error_message = 'Это поле должно быть заполнено';
+            break;
+        case (is_strong_password($value)):
+            $error_message = 'Пароль должен содержать не менее 6 символов, 
+                в нем должны быть цифры и латинские буквы верхнего и нижнего регистров';
             break;
     }
 
